fcc82e231b9eea9d9d5ec4592efff603341472be
Chris Madge <chris@madg.es>
2021-09-01 15:14:26 +0100

    .

--- story/script.js
+++ story/script.js
@@ -1,18 +1,25 @@
-console.clear()
 var $carousel = $('.carousel').flickity({
 	// selectedAttraction: 0.3,
 	// friction: 0.6
 })
-var content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.';
 
-var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+function typewritereffect($el) {
+  const characters = $el.text().split('');
+  $el.text('');
+  characters.forEach(function(character) { 
+    const $span = $("<span>");
+    $span.text(character);
+    $span.css({opacity:0}).appendTo($el);
+    });
+  const $spans = $("span", $el);
+  $spans.each((i, el) => $(el).delay(100*i).animate({opacity: 1}), 100);
+}
 
+function runtypewriters() {
+  $("p.typewriter").each((i,el) => typewritereffect($(el)));
+}
 
-$(ele).hide().appendTo('p').each(function (i) {
-    $(this).delay(100 * i).css({
-        display: 'inline',
-        opacity: 0
-    }).animate({
-        opacity: 1
-    }, 100);
+$carousel.on('change.flickity', function() {
+  runtypewriters();
 });
+runtypewriters();
