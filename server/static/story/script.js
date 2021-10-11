e507f4d34d8f4a8ba178c8e048f5f85f80960b2c
Chris Madge <chris@madg.es>
2021-10-11 10:50:44 +0100

    .

---
+++ server/static/story/script.js
@@ -0,0 +1,26 @@
+var $carousel = $('.carousel').flickity({
+	// selectedAttraction: 0.3,
+	// friction: 0.6
+})
+
+function typewritereffect($el) {
+  const characters = $el.text().split('');
+  $el.text('');
+  characters.forEach(function(character) { 
+    const $span = $("<span>");
+    $span.text(character);
+    $span.css({opacity:0}).appendTo($el);
+    });
+  const $spans = $("span", $el);
+  $spans.each((i, el) => $(el).delay(40*i).animate({opacity: 1}), 100);
+}
+
+function runtypewriters() {
+  $("p.typewriter").each((i,el) => typewritereffect($(el)));
+}
+
+$carousel.on('change.flickity', function() {
+  runtypewriters();
+});
+runtypewriters();
+
--- update-story.sh
+++ update-story.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 TMPDIR=$(mktemp -d)
+ASSET_DEST="server/static/story"
 ASSET_PATH="story"
 TEMPLATE_PATH="server/templates/story"
 
 git clone git@github-qmul:falthani/lingotowns-story.git $TMPDIR
 
-mkdir -p $ASSET_PATH
+mkdir -p $ASSET_DEST
 mkdir -p $TEMPLATE_PATH
 
 perl -ne "s/src=\"(.*?\.json)\"/src=\"\/$ASSET_PATH\/\1\"/g;print" $TMPDIR/dist/index.html |\
        	perl -ne "s/\"([^\"]*?script\.js)\"/\"\/$ASSET_PATH\/script.js\"/g;print" |\
        	perl -ne "s/\"([^\"]*?style\.css)\"/\"\/$ASSET_PATH\/style.css\"/g;print" > $TEMPLATE_PATH/index.html
 
-mv $TMPDIR/dist/*.json $ASSET_PATH
-mv $TMPDIR/dist/*.js $ASSET_PATH
-mv $TMPDIR/dist/*.css $ASSET_PATH
+mv $TMPDIR/dist/*.json $ASSET_DEST
+mv $TMPDIR/dist/*.js $ASSET_DEST
+mv $TMPDIR/dist/*.css $ASSET_DEST
 
-git add $TEMPLATE_PATH $ASSET_PATH
+git add $TEMPLATE_PATH $ASSET_DEST
