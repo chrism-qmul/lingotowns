dea819af64084b88f972e686c8c9fe4a81c78f10
Fatima Althani <timaalthani@gmail.com>
2022-06-28 15:25:12 +0100

    link fix for tutorial screens

--- server/app.py
+++ server/app.py
@@ -185,6 +185,23 @@ def info():
 #     else:
 #         return redirect(AUTH_SERVER + "/login-as-guest?redirect=" + HOSTNAME)
 
+@app.route("/play")
+def lingotowns():
+    auth_token = request.args.get("auth_token")
+    session_auth = session.get('auth')
+    if auth_token:
+        session['auth'] = auth_from_token(auth_token)
+        session['auth_token'] = auth_token
+        return redirect("/")
+    elif session_auth:
+        if True:#session.get('seen_intro'):
+            return render_template("game.html", auth_server=AUTH_SERVER, is_guest=(session_auth['username'] == "Guest"))
+        else:
+            return redirect("/intro")
+    else:
+        return redirect(AUTH_SERVER + "/login-as-guest?redirect=" + HOSTNAME)
+
+
 @app.route("/")
 def intro():
     session['seen_intro'] = True
